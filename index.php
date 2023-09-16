c58ea0155da01c72302f53f612bab149df79e1e6
caua-pereira-santos <cauap600@gmail.com>
2023-09-16 15:09:56 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema - AW2</title>
+    <!-- Conexão com o Bootstrap - para deixar o formulário de login bonitinho -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+     
+
+</head>
+<body>
+    <!-- Formulário de login -->
+
+   <div class="container">
+        <div class="row">
+            <div class="col-lg-4 offset-lg-4">
+               <div class="card">
+                <div class="card-body">
+                    <h3>Faça Login - Motomami</h3>
+                </div>
+              <div class="card-body">
+              <form action="login.php" method="POST">
+                    <div>
+                        <div class="mb-3">
+                            <label>Usuário</label>
+                            <input type="text" name="usuario" class="form-control">
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="mb-3">
+                        <label>Senha</label>
+                        <input type="password" name="senha" class="form-control">
+                        </div>
+                    </div>
+                    <!-- Botão de enviar que submete o forms para login.php-->
+                    <div>
+                        <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </div>
+                </form>
+              </div>
+               </div>
+            </div>
+        </div>
+    </div>
+   
+    
+</body>
+</html>
