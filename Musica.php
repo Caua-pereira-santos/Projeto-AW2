e01db824102535f942816ea7c53a6845767b1784
Med <101737670+Caua-pereira-santos@users.noreply.github.com>
2023-09-28 10:25:16 -0300

    Add: Comentários

--- Musica.php
+++ Musica.php
@@ -1,3 +1,8 @@
+
+<!--Class Album inicia a classe--> 
+  
+ <!--As propriedades são privadas,por isso elas só podem ser acessadas e modificadas dentro da classe--> 
+ 
 <?php
 class Musica {
     private $id;
@@ -5,12 +10,16 @@ class Musica {
     private $duracao;
     private $album;
 
+//Método construtor 
+
     public function __construct($id, $nome, $duracao, Album $album) {
         $this->id = $id;
         $this->nome = $nome;
         $this->duracao = $duracao;
         $this->album = $album;
     }
+
+//Retorna os valores nome,duração e album 
 
     public function getNome() { return $this->nome; }
     public function getDuracao() { return $this->duracao; }
