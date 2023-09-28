477bebff7e1760b49bfc2f2af343657b2a03d57b
Med <101737670+Caua-pereira-santos@users.noreply.github.com>
2023-09-28 10:23:31 -0300

    Add: Comentários

--- Gravadora.php
+++ Gravadora.php
@@ -1,14 +1,23 @@
+
+<!--Class Album inicia a classe--> 
+  
+ <!--As propriedades são privadas,por isso elas só podem ser acessadas e modificadas dentro da classe--> 
+ 
 <?php
 class Gravadora {
     private $id;
     private $nome;
     private $endereco;
 
+//Método construtor 
+
     public function __construct($id, $nome, $endereco) {
         $this->id = $id;
         $this->nome = $nome;
         $this->endereco = $endereco;
     }
+
+//Retorna os valores Id,nome e endereço 
 
     public function getId() { return $this->id; }
     public function getNome() { return $this->nome; }
